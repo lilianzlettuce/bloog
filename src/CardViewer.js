562dc559e4a4b21ef6cca075a0f26a3f82d57a26
Lilian Zhao <milanzq309@gmail.com>
2021-04-26 00:18:44 -0700

    added flipcard function

--- src/CardViewer.js
+++ src/CardViewer.js
@@ -5,20 +5,34 @@ export default class CardViewer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      currentCard: 0,
+      front: true,
+    }
+  }
 
+  flipCard = e => {
+    if (this.state.front) {
+      e.target.style.transform = 'rotateX(180deg)'
+      let text = document.querySelector('#text-display')
+      text.textContent = this.props.cards[this.state.currentCard].back
+      this.setState({ front: false })
+    } else {
+      e.target.style.transform = 'rotateX(0deg)'
+      this.setState({ front: true })
     }
   }
 
   render() {
     return (
       <div className="container">
-        <div class="heading">
+        <div className="heading">
           <h2></h2>
           <button className="switch-btn" onClick={this.props.switchMode}>Edit Cards →</button>
         </div>
         <div id="card-container">
-          <div id="card">
-            <div>{this.props.cards[0].front}</div>
+          <div id="card" onClick={this.flipCard}>
+            <div id="text-display-front" >{this.props.cards[this.state.currentCard].front}</div>
+            <div id="text-display-back" >{this.props.cards[this.state.currentCard].back}</div>
           </div>
           <div id="controls-container">
             <h3>Progress</h3>
