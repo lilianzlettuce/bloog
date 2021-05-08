1f30ed36f83f8a731d2fe53ab522d1b19392e06e
Lilian Zhao <milanzq309@gmail.com>
2021-05-08 00:34:23 -0700

    finished styling home page btns

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,9 +8,14 @@ export default function HomePage() {
             <div className="section">
                 <Link className="btn-container" to="/viewer">
                     <div className="btn-header">Study Cards</div>
+                    <svg id="cards-icon" width="170px" height="120px">
+                        <rect width="110px" height="80px" x="40" y="10" rx="10" ry="10" stroke="rgb(24, 40, 255)"  strokeLinecap="round" strokeWidth="4" fill="white"/>
+                        <rect width="110px" height="80px" x="20" y="25" rx="10" ry="10" stroke="rgb(24, 40, 255)"  strokeLinecap="round" strokeWidth="4" fill="white"/>
+                    </svg>
                 </Link>
                 <Link className="btn-container" to="/editor">
                     <div className="btn-header">Edit Cards</div>
+                    <i className="fas fa-marker btn-icon"></i>
                 </Link>
             </div>
         </div>
