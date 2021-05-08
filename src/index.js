746095c781e780f645d330c5462732f6406a80ef
Lilian Zhao <milanzq309@gmail.com>
2021-05-08 02:08:04 -0700

    set up redux & firebase

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import App from './App'
 
 import { BrowserRouter } from 'react-router-dom'
 
+import { Provider } from 'react-redux'
 import firebase from 'firebase/app'
+import { createStore, combineReducers } from 'redux'
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer
+} from 'react-redux-firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -21,9 +27,36 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer
+  // firestore: firestoreReducer // <- needed if using firestore
+})
+
+// Create store with reducers and initial state
+const store = createStore(rootReducer)
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users'
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  // enableClaims: true // Get custom claims along with the profile
+}
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch
+  // createFirestoreInstance // <- needed if using firestore
+}
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.querySelector('#root')
 );
