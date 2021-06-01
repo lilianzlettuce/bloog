6fd25dd2ce7f7bc306d0724d7db55603dba5d5f2
Lilian Zhao <milanzq309@gmail.com>
2021-05-31 19:09:45 -0700

    test

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,20 +2,33 @@ import './HomePage.css'
 
 import TopBar from './TopBar'
 
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
+import { firebaseConnect, isEmpty, isLoaded } from 'react-redux-firebase'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
 
-export default function HomePage() {
+const HomePage = (props) => {
+    const decks = this.props.decks.map((deck, index) => {
+        <Link className="btn-container" to="/viewer">
+
+        </Link>
+    })
+
     return (
         <div id="main">
             <TopBar />
             <div className="section">
-                <Link className="btn-container" to="/viewer">
-                    
-                </Link>
-                <Link className="btn-container" to="/editor">
-                    <i className="fas fa-marker btn-icon"></i>
-                </Link>
+                {decks}
             </div>
         </div>
     )
 }
+
+const mapStateToProps = (state, props) => {
+    
+}
+
+export default compose(
+    firebaseConnect([{path: '/homepage'}]),
+    connect(mapStateToProps),
+)(HomePage)
