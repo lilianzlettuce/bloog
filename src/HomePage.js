fba111e3febb34a1d6041969bae02e187d01d948
Lilian Zhao <milanzq309@gmail.com>
2021-05-31 18:00:51 -0700

    topbar

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,19 +1,16 @@
 import './HomePage.css'
 
+import TopBar from './TopBar'
+
 import { Link } from 'react-router-dom'
 
 export default function HomePage() {
     return (
         <div id="main">
-            <div className="section">
-                <h1 id="bg">BLOOG</h1>
-            </div>
+            <TopBar />
             <div className="section">
                 <Link className="btn-container" to="/viewer">
-                    <svg id="cards-icon" width="170px" height="120px">
-                        <rect class="cards-path" width="110px" height="80px" x="40" y="10" rx="10" ry="10" stroke="rgb(24, 40, 255)"  strokeLinecap="round" strokeWidth="4" fill="white"/>
-                        <rect class="cards-path" width="110px" height="80px" x="20" y="25" rx="10" ry="10" stroke="rgb(24, 40, 255)"  strokeLinecap="round" strokeWidth="4" fill="white"/>
-                    </svg>
+                    
                 </Link>
                 <Link className="btn-container" to="/editor">
                     <i className="fas fa-marker btn-icon"></i>
--- src/TopBar.js
+++ src/TopBar.js
@@ -1,9 +1,16 @@
 import './TopBar.css'
 
-const TopBar = () => {
+import { Link } from 'react-router-dom'
+
+export default function TopBar(props) {
     return (
-        <div>
-            
+        <div id="topbar">
+            <Link to="/">
+                <svg id="cards-icon" width="170px" height="120px">
+                    <rect class="cards-path" width="110px" height="80px" x="40" y="10" rx="10" ry="10" stroke="rgb(24, 40, 255)"  strokeLinecap="round" strokeWidth="4" fill="white"/>
+                    <rect class="cards-path" width="110px" height="80px" x="20" y="25" rx="10" ry="10" stroke="rgb(24, 40, 255)"  strokeLinecap="round" strokeWidth="4" fill="white"/>
+                </svg>
+            </Link>
         </div>
     )
 }
